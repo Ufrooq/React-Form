9cabba59e00c89db19945f4d489626dbeab93431
Ufrooq <muhammadshafqatali3@gmail.com>
2022-07-26 20:06:21 +0500

    xdcfdsxc

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Card from './components/Card/Card';
+import Snow from './components/Apps/snow';
+function App() {
+  return (
+    <>
+      <Snow />
+    </>
+  );
+}
+
+export default App;
